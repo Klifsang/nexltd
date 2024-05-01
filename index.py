0d09ff3d0ccc593c52cf4e0aaf0dd7c857dca2b5
Klif <sang.wicklife@student.moringaschool.com>
2024-05-01 10:18:51 +0300

    initial commit

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, send_from_directory
 
 app = Flask(
     __name__, 
-    static_folder='../client/dist', 
+    static_folder='./client/dist', 
     static_url_path='')
 
 @app.route('/')

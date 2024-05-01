acb79ba72fe40934716b50fa9a06c31db288f022
Klif <sang.wicklife@student.moringaschool.com>
2024-05-01 10:23:47 +0300

    first fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [aboutMessage, setAboutMessage] = useState('');
+
+  useEffect(() => {
+    console.log('data.message')
+    fetch('/about') // Relative path to your API route
+      .then(response => response.json())
+      .then(data => {
+        setAboutMessage(data.about)
+        console.log(data.about)
+      })
+      .catch(error => console.error('Error fetching about page:', error));
+  }, []);
 
   return (
     <>
@@ -27,6 +39,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p className="read-the-docs">
+        {aboutMessage}
       </p>
     </>
   )
